b7ba1216f534107b70cb451c0794e97e1ddbf4ec
garshishka <garshishka@yandex.ru>
2022-07-01 16:27:04 +0300

    Fix for "...11" case

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main() {
-    val likes = 61
-    val peopleWord = if(likes.toString()[likes.toString().length-1] == '1') " человеку" else " людям"
+    val likes = 111
+    val likesAsString = likes.toString()
+    var endOnlyInOne =if(likesAsString[likesAsString.length-1] =='1') true else false //checking if last char is 1
+    val moreThanOneChar = if(likesAsString.length>1) true else false //making bool so the next line works for 0..9
+    if(moreThanOneChar && likesAsString[likesAsString.length - 2] == '1') endOnlyInOne = false //if it's "....11"
+
+    val peopleWord = if(endOnlyInOne) " человеку" else " людям"
 
     println("Понравилось $likes$peopleWord")
 }
